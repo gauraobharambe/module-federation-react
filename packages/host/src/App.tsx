ccec16404d8c8fe7b456ebe81db1859fcecbe899
Gaurao Bharambe <69198127+gauraobharambe@users.noreply.github.com>
2024-01-15 13:18:43 +0530

    Update App.tsx

--- packages/host/src/App.tsx
+++ packages/host/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <div className="card" style={{placeItems:'center'}}>
         <h1>Module Federation with Vite + React</h1>
         <p>
-          On this page,<br/>
+          In this App,<br/>
           Header Component is a rendered from remote <a href='http://localhost:4201' target='_blank'>http://localhost:4201</a>.<br/>
           List Component is a rendered from remote <a href='http://localhost:4202' target='_blank'>http://localhost:4202</a>.
         </p>
