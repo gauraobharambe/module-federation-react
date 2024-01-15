107ff685728e2198994112b1c48c0b1f219d6f11
Gaurao Bharambe <gaurao7793@gmail.com>
2024-01-15 13:29:47 +0530

    Minor Changes

--- packages/host/src/App.tsx
+++ packages/host/src/App.tsx
@@ -7,17 +7,19 @@ function App() {
   return (
     <>
       <Header />      
-      <div className="card" style={{placeItems:'center'}}>
-        <h1>Module Federation with Vite + React</h1>
-        <p>
-          In this App,<br/>
-          Header Component is a rendered from remote <a href='http://localhost:4201' target='_blank'>http://localhost:4201</a>.<br/>
-          List Component is a rendered from remote <a href='http://localhost:4202' target='_blank'>http://localhost:4202</a>.
-        </p>
-        <div className='user-list'>
-          <Users />
-        </div>
-      </div> 
+      <main style={{margin:'5px auto'}}>
+        <div className="card" style={{placeItems:'center'}}>
+          <h1>Module Federation with Vite + React</h1>
+          <p>
+            In this App,<br/>
+            Header Component is a rendered from remote <a href='http://localhost:4201' target='_blank'>http://localhost:4201</a>.<br/>
+            List Component is a rendered from remote <a href='http://localhost:4202' target='_blank'>http://localhost:4202</a>.
+          </p>
+          <div className='user-list'>
+            <Users />
+          </div>
+        </div> 
+      </main>
     </>
   )
 }
